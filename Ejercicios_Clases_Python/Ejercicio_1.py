d7474b78c282079289272e062f0a4bc5fa2d882a
JaimeAFL <jferrala@gmail.com>
2025-09-01 17:33:22 +0000

    commit

---
+++ Ejercicios_Clases_Python/Ejercicio_1.py
@@ -0,0 +1,19 @@
+entrada = input ("introduzca un número entero positivo por favor")
+if not entrada.isdigit():
+    print ("El dato introducido no es número. Por favor, vuelva a intentarlo")
+else:
+    numero = int(entrada)
+
+if numero <= 0:
+    print ("No ha introducido un número entero positivo. Por favor, vuelva a intentarlo")
+
+def primo (n):
+    if n == 1:
+        print("Noes un numero primo")
+        return
+    for i in range (2, n):
+        if n % i == 0:
+            print ("No es primo")
+            return
+    
+    print("Es primo")
